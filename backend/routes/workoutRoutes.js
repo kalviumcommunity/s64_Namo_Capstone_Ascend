f0cc41982e0d41af8c37a03fb7c6755dd2a530eb
NamoxKalvium <Namonarayan.Mina@Kalvium.community>
2025-04-17 00:48:30 +0530

    get api done

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -21,4 +21,36 @@ router.post('/', async (req, res) => {
   }
 });
 
+// router.get("/", async (req, res) => {
+//     try {
+//       const workouts = await Workout.find().populate("user", "email"); // populate user info (optional)
+//       res.json(workouts);
+//     } catch (err) {
+//       res.status(500).json({ message: err.message });
+//     }
+//   });
+
+  router.get("/:id", async (req, res) => {
+    try {
+      const workout = await Workout.findById(req.params.id).populate("user", "email");
+      if (!workout) return res.status(404).json({ message: "Workout not found" });
+      res.json(workout);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+  // GET /api/workouts/user/:userId
+router.get('/user/:userId', async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const workouts = await Workout.find({ user: userId }).populate("user", "name email");
+      res.status(200).json(workouts);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Server Error" });
+    }
+  });
+  
+
 module.exports = router;
